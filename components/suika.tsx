a038a0e3061aba7e5d8a9c5350f84122d899edf2
김현술 <devsepnine@mrblue.com>
2023-11-10 18:09:59 +0900

    fix: 아이템 생성 코드 수정

--- components/suika.tsx
+++ components/suika.tsx
@@ -18,6 +18,8 @@ const Suika = () => {
 
     // Matter.js 엔진 생성
     const engine = Engine.create();
+    engine.gravity.x = 0;
+    engine.gravity.y = 1;
     // runner create
     const runner = Runner.create();
     // 렌더러 생성
@@ -78,7 +80,8 @@ const Suika = () => {
 
     const addWatermelon = (x: number, y: number) => {
       const watermelon = Bodies.circle(x, y, 20, {
-        restitution: 0.1,
+        restitution: 0.5,
+        mass: 1,
         label: "watermelon" + watermelons.length,
         render: {
           sprite: {
@@ -92,12 +95,69 @@ const Suika = () => {
       watermelons.push(watermelon);
     };
 
+    const addLemon = (x: number, y: number) => {
+      const lemon = Bodies.circle(x, y, 25, {
+        restitution: 0.5,
+        mass: 2,
+        label: "lemon" + lemons.length,
+        render: {
+          sprite: {
+            texture: "./lemon.png",
+            xScale: (25 * 2) / 100,
+            yScale: (25 * 2) / 100,
+          },
+        },
+      });
+      World.add(world, lemon);
+      lemons.push(lemon);
+    };
+
+    const addPodo = (x: number, y: number) => {
+      const grapeVertices = [
+        { x: 60, y: 0 },
+        { x: 115, y: 22 },
+        { x: 112, y: 45 },
+        { x: 98, y: 67 },
+        { x: 93, y: 108 },
+        { x: 2, y: 119 },
+        { x: 13, y: 34 },
+        { x: 53, y: 22 },
+        { x: 58, y: 2 },
+      ];
+      const podo = Bodies.fromVertices(x, y, [grapeVertices], {
+        restitution: 0.5,
+        mass: 20,
+        label: "podo" + podos.length,
+        render: {
+          sprite: {
+            texture: "grape.png",
+            xScale: (60 * 2) / 100,
+            yScale: (60 * 2) / 100,
+          },
+        },
+      });
+      World.add(world, podo);
+      podos.push(podo);
+    };
+
     canvasRef.current?.addEventListener("click", (e) => {
       const bound = canvasRef.current?.getBoundingClientRect();
       const mouseX = e.clientX - (bound?.left || 0);
-      const mouseY = e.clientY - (bound?.top || 0);
+      const mouseY = 100; // e.clientY - (bound?.top || 0);
 
-      addWatermelon(mouseX, mouseY);
+      const random = Math.floor(Math.random() * 3);
+
+      switch (random) {
+        case 0:
+          addWatermelon(mouseX, mouseY);
+          break;
+        case 1:
+          addLemon(mouseX, mouseY);
+          break;
+        case 2:
+          addPodo(mouseX, mouseY);
+          break;
+      }
     });
 
     const convertToLemon = (watermelons: Matter.Body[]) => {
@@ -108,31 +168,15 @@ const Suika = () => {
         const positionA = watermelons[s1].position;
         const positionB = watermelons[s2].position;
 
+        // 두 수박을 제거합니다.
+        Composite.remove(world, [watermelons[0], watermelons[1]]);
+
         // 두 수박 사이의 중간 지점을 계산합니다.
         const centerX = (positionA.x + positionB.x) / 2;
         const centerY = (positionA.y + positionB.y) / 2;
 
-        // 중간 지점에 레몬을 생성합니다.
-        const lemon = Bodies.circle(centerX, centerY, 25, {
-          restitution: 0.3,
-          label: "lemon" + lemons.length,
-          render: {
-            sprite: {
-              texture: "./lemon.png",
-              xScale: (25 * 2) / 100,
-              yScale: (25 * 2) / 100,
-            },
-          },
-        });
-
-        // 두 수박을 제거합니다.
-        Composite.remove(world, [watermelons[0], watermelons[1]]);
-
         // 레몬을 월드에 추가합니다.
-        World.add(world, lemon);
-
-        // 레몬 배열에 추가합니다.
-        lemons.push(lemon);
+        addLemon(centerX, centerY);
       }
     };
 
@@ -140,48 +184,19 @@ const Suika = () => {
       if (lemons.length >= 2) {
         const s1 = lemons.indexOf(lemons[0]);
         const s2 = lemons.indexOf(lemons[1]);
-        // 두 수박의 위치를 가져옵니다.
+        // 두 레몬의 위치를 가져옵니다.
         const positionA = lemons[s1].position;
         const positionB = lemons[s2].position;
 
-        // 두 수박 사이의 중간 지점을 계산합니다.
+        // 두 레몬을 제거합니다.
+        Composite.remove(world, [lemons[0], lemons[1]]);
+
+        // 두 레몬 사이의 중간 지점을 계산합니다.
         const centerX = (positionA.x + positionB.x) / 2;
         const centerY = (positionA.y + positionB.y) / 2;
 
-        const grapeRadius = 60;
-        const grapeImageSize = 100;
-        const grapeVertices = [
-          { x: 60, y: 0 },
-          { x: 115, y: 22 },
-          { x: 112, y: 45 },
-          { x: 98, y: 67 },
-          { x: 93, y: 108 },
-          { x: 2, y: 119 },
-          { x: 13, y: 34 },
-          { x: 53, y: 22 },
-          { x: 58, y: 2 },
-        ];
-        // 중간 지점에 포도를 생성합니다.
-        const podo = Bodies.fromVertices(centerX, centerY, [grapeVertices], {
-          restitution: 0.6,
-          label: "podo" + podos.length,
-          render: {
-            sprite: {
-              texture: "grape.png",
-              xScale: (grapeRadius * 2) / grapeImageSize,
-              yScale: (grapeRadius * 2) / grapeImageSize,
-            },
-          },
-        });
-
-        // 두 수박을 제거합니다.
-        Composite.remove(world, [lemons[0], lemons[1]]);
-
-        // 레몬을 월드에 추가합니다.
-        World.add(world, podo);
-
-        // 레몬 배열에 추가합니다.
-        podos.push(podo);
+        // 포도를 월드에 추가합니다.
+        addPodo(centerX, centerY);
       }
     };
 
@@ -199,7 +214,7 @@ const Suika = () => {
         }
       });
       if (collidingWatermelons.length >= 2) {
-        // 두 개의 레몬이 충돌했을 때 포도으로 변경
+        // 두 개의 레몬이 충돌 했을 때 포도으로 변경
         convertToLemon(collidingWatermelons);
       } else if (collidingLemons.length >= 2) {
         convertToPodo(collidingLemons);
